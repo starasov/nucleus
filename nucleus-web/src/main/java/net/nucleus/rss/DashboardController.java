21242bdcd6d3d270ddc7fe794edb79c2d1999703
starasov <tarasov.inf@gmail.com>
2013-05-05 01:09:46 -0400

    - Added dummy feed entry model - Added dummy dashboard controller - Added dummy welcome controller - Added slf4j + logging configuration - Updated spring context xml to support annotated components - Created initial static resources folder structure - Setup initial static resources handling - Setup MVC view resolution

---
+++ nucleus-web/src/main/java/net/nucleus/rss/DashboardController.java
@@ -0,0 +1,34 @@
+package net.nucleus.rss;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * User: starasov
+ * Date: 5/1/13
+ * Time: 8:20 PM
+ */
+@Controller
+@RequestMapping("/dashboard")
+public class DashboardController {
+    private static final Logger logger = LoggerFactory.getLogger(DashboardController.class);
+
+    @RequestMapping("/")
+    public ModelAndView latest() {
+        logger.debug("[{}][index] - begin", this.getClass().getSimpleName());
+
+        List<FeedEntry> list = new ArrayList<FeedEntry>();
+
+        ModelAndView modelAndView = new ModelAndView("index");
+        modelAndView.addObject("entries", list);
+
+        logger.debug("[{}][index] - end", this.getClass().getSimpleName());
+        return modelAndView;
+    }
+}
---
+++ nucleus-web/src/main/java/net/nucleus/rss/FeedEntry.java
@@ -0,0 +1,70 @@
+package net.nucleus.rss;
+
+import java.util.Date;
+
+/**
+ * User: starasov
+ * Date: 5/1/13
+ * Time: 8:18 PM
+ */
+public class FeedEntry {
+    private String title;
+    private String shortDescription;
+    private String fullDescription;
+    private String externalUrl;
+
+    private Date publicationDate;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getFullDescription() {
+        return fullDescription;
+    }
+
+    public void setFullDescription(String fullDescription) {
+        this.fullDescription = fullDescription;
+    }
+
+    public String getExternalUrl() {
+        return externalUrl;
+    }
+
+    public void setExternalUrl(String externalUrl) {
+        this.externalUrl = externalUrl;
+    }
+
+    public Date getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(Date publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder();
+        sb.append("FeedEntry");
+        sb.append("{title='").append(title).append('\'');
+        sb.append(", shortDescription='").append(shortDescription).append('\'');
+        sb.append(", fullDescription='").append(fullDescription).append('\'');
+        sb.append(", externalUrl='").append(externalUrl).append('\'');
+        sb.append(", publicationDate=").append(publicationDate);
+        sb.append('}');
+        return sb.toString();
+    }
+}
